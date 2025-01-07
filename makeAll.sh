06635c5c2817861affe91d4d425ea14d9db1e65d
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-01-07 13:07:26 -0500

    add nofx2EventReader and also pass arguments to make

--- makeAll.sh
+++ makeAll.sh
@@ -2,15 +2,16 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CUR_DIR=`pwd`
-
 cd ${SCRIPT_DIR}/libmcp
-make
+make $@
 cd ${SCRIPT_DIR}/libmcp2221-master/libmcp2221/
-make
+make $@
 cd ${SCRIPT_DIR}/mcpi2c_wr
-make
+make $@
 cd ${SCRIPT_DIR}/nofx2ControlLib
-make
+make $@
 cd ${SCRIPT_DIR}/nofx2ControlReader
-make
+make $@
+cd ${SCRIPT_DIR}/nofx2EventReader
+make $@
 cd ${CUR_DIR}
