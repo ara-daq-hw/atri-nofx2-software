d3cf1acd5ab5882cfcb07feace026398ea0e2ebe
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-01-07 13:06:50 -0500

    fix debug printing and add prefixes

--- nofx2ControlReader/nofx2ControlReader.c
+++ nofx2ControlReader/nofx2ControlReader.c
@@ -10,6 +10,8 @@
 #include <errno.h>
 #include <signal.h>
 
+#define LOG_PFX "nofx2ControlReader: "
+
 // this program should be burnt and thrown into the pits of hell
 // ... but failing that, you launch it just by
 // nofx2ControlReader [verbosity#]
@@ -29,6 +31,8 @@ int main(int argc, char **argv) {
   unsigned int nrdb;
   uint8_t buffer[256];
   uint8_t *dp;
+  uint8_t *op;
+
   int verbosity = 0;
 
   argc--;
@@ -36,15 +40,15 @@ int main(int argc, char **argv) {
   if (argc) verbosity = strtoull(*argv, NULL, 0);
 
   retval = mkfifo("/tmp/atri_inpkts", 0666);
-  if (verbosity > 1) printf("mkfifo returned retval %d\n", retval);
+  if (verbosity > 1) printf(LOG_PFX "mkfifo returned retval %d\n", retval);
   // this will block until someone opens us
   wfd = open("/tmp/atri_inpkts", O_WRONLY);
   // ok we should be open now
   rfd = open("/dev/xillybus_pkt_out", O_RDONLY);
   if (wfd < 0 || rfd < 0) {
-    fprintf(stderr, "cannot start up, one of wfd/rfd open() failed\n");
-    fprintf(stderr, "open(/tmp/atri_inpkts): %d", wfd);
-    fprintf(stderr, "open(/dev/xillybus_pkt_out): %d", rfd);
+    fprintf(stderr, LOG_PFX "cannot start up, one of wfd/rfd open() failed\n");
+    fprintf(stderr, LOG_PFX "open(/tmp/atri_inpkts): %d", wfd);
+    fprintf(stderr, LOG_PFX "open(/dev/xillybus_pkt_out): %d", rfd);
     unlink("/tmp/atri_inpkts");
     exit(1);
   }
@@ -55,20 +59,20 @@ int main(int argc, char **argv) {
     while (ndb) {
       retval = read(rfd, dp, ndb);
       if (retval < 0) {
-	fprintf(stderr, "header: got a read error %d\n", errno);
+	fprintf(stderr, LOG_PFX "header: got a read error %d\n", errno);
 	goto myExit;
       }
       ndb -= retval;
       dp += retval;
       if (verbosity > 1) {
-	printf("got %d bytes reading header, %d left\n", retval, ndb);
+	printf(LOG_PFX "got %d bytes reading header, %d left\n", retval, ndb);
 	if (verbosity > 2) {
-	  printf("header currently: %2.2x %2.2x %2.2x %2.2x\n",
+	  printf(LOG_PFX "header currently: %2.2x %2.2x %2.2x %2.2x\n",
 		 buffer[0],
 		 buffer[1],
 		 buffer[2],
 		 buffer[3]);
-	  printf("dp offset is %d\n", (dp-buffer));
+	  printf(LOG_PFX "dp offset is %d\n", (dp-buffer));
 	}
       }
     }
@@ -76,25 +80,30 @@ int main(int argc, char **argv) {
     nrdb = ndb;
     dp = buffer + 4;
     if (verbosity > 1) {
-      printf("got a header: %2.2x %2.2x %2.2x %2.2x\n",
+      printf(LOG_PFX "got a header: %2.2x %2.2x %2.2x %2.2x\n",
 	     buffer[0], buffer[1], buffer[2], buffer[3]);
-      printf("waiting for %d bytes (dp offset %d)\n", ndb,
+      printf(LOG_PFX "waiting for %d bytes (dp offset %d)\n", ndb,
 	     (dp-buffer));
     }
     while (ndb) {
       retval = read(rfd, dp, ndb);
       if (retval < 0) {
-	fprintf(stderr, "got a read error %d\n", errno);
+	fprintf(stderr, LOG_PFX "got a read error %d\n", errno);
 	goto myExit;
       }
       ndb -= retval;
+      op = dp;
       dp += retval;
       if (verbosity > 1) {
-	printf("got %d bytes (dp offset now %d) remaining %d\n", retval, (dp-buffer), ndb);
+	printf(LOG_PFX "got %d bytes (dp offset now %d) remaining %d\n", retval, (dp-buffer), ndb);
 	if (verbosity > 2) {
+	  printf(LOG_PFX);
 	  for (int i=0;i<(nrdb-ndb);i++) {
-	    printf("%2.2x", dp[i]);
-	    if (!((i+1)%16)) printf("\n");
+	    printf("%2.2x", op[i]);
+	    if (!((i+1)%16)) {
+	      if (i+1<(nrdb-ndb)) printf(LOG_PFX);
+	      printf("\n");
+	    }
 	    else if (i+1<(nrdb-ndb)) printf(" ");
 	    else printf("\n");
 	  }
@@ -102,23 +111,29 @@ int main(int argc, char **argv) {
       }
     }
     nb = buffer[3] + 5;
-    if (verbosity > 0) {
-      printf("got a packet (%d bytes) - delivering\n", nb);
-      for (int i=0;i<nb;i++) {
-	printf("%2.2x", buffer[i]);
-	if (!((i+1)%16)) printf("\n");
-	else if (i+1<nb) printf(" ");
-	else printf("\n");
+    if (verbosity > 0) {      
+      printf(LOG_PFX "got a packet (%d bytes) - delivering\n", nb);
+      if (verbosity > 1) {
+	printf(LOG_PFX);
+	for (int i=0;i<nb;i++) {
+	  printf("%2.2x", buffer[i]);
+	  if (!((i+1)%16)) {
+	    if (i+1<nb) printf(LOG_PFX);
+	    printf("\n");
+	  }
+	  else if (i+1<nb) printf(" ");
+	  else printf("\n");
+	}
       }
     }
     retval = write(wfd, buffer, nb);
     if (retval < 0) {
-      fprintf(stderr, "got a write error %d\n", retval);
+      fprintf(stderr, LOG_PFX "got a write error %d\n", retval);
       break;
     }
   }
  myExit:
-  printf("Exiting\n");
+  printf(LOG_PFX "Exiting\n");
   close(rfd);
   close(wfd);
   // destroy on exit
